334192bc9a075959027a2ba2b87328fe1400268d
mamuu21 <mariamumussa071@gmail.com>
2026-01-14 15:06:32 +0300

    new updates

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -4,6 +4,9 @@ DEBUG = False
 
 ALLOWED_HOSTS = [
     "vemacars-backend.deploy.tz",
+    ".deploy.tz",          # 👈 VERY important (wildcard subdomains)
+    "localhost",
+    "127.0.0.1",
 ]
 
 CORS_ALLOWED_ORIGINS = [
